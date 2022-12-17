93223d42a0ddd654480579c83a2f83fb2eae603f
Mehmet Usta <ustamehmet83@hotmail.com>
2022-12-17 19:15:50 +0300

    initial

--- src/week15_17_12_2022/task1/GarantiBank.java
+++ src/week15_17_12_2022/task1/GarantiBank.java
@@ -1,4 +1,9 @@
 package week15_17_12_2022.task1;
 
 public class GarantiBank extends Bank{
+
+    @Override
+    public void calcTheInterest() {
+        System.out.println(getCapital()*8/1000);
+    }
 }
